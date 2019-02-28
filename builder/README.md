a342219c7280bb06a80434961005cbcc78cc5082
Rangga Adhitya Prawira <deneuv3.4@gmail.com>
2019-02-28 21:26:03 +0700

    add builder pattern

---
+++ builder/README.md
@@ -0,0 +1,3 @@
+# Builder Pattern
+
+The Builder Pattern is a creational design pattern that’s used to encapsulate the construction logic for an object. It is often used when the construction process of an object is complex. The patterns is well suited for constructing different representations of the same class. In this example I create a Message builder that need header, body, and footer to send message.
---
+++ builder/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// Message Json struct
+type Message struct {
+	Body   []byte
+	Format string
+}
+
+// MessageBuilder interface
+type MessageBuilder interface {
+	SetHeader(header string)
+	SetBody(body string)
+	SetFooter(footer string)
+	BuildMessage() (*Message, error)
+}
+
+// MessageStruct type
+type MessageStruct struct {
+	Header string
+	Body   string
+	Footer string
+}
+
+// SetHeader Function
+func (s *MessageStruct) SetHeader(header string) {
+	s.Header = header
+}
+
+// SetBody function
+func (s *MessageStruct) SetBody(body string) {
+	s.Body = body
+}
+
+// SetFooter function
+func (s *MessageStruct) SetFooter(footer string) {
+	s.Footer = footer
+}
+
+// BuildMessage function to build the Message Struct into Message struct
+func (s *MessageStruct) BuildMessage() (*Message, error) {
+	m := make(map[string]string)
+	m["header"] = s.Header
+	m["body"] = s.Body
+	m["footer"] = s.Footer
+
+	data, err := json.Marshal(m)
+	if err != nil {
+		return nil, err
+	}
+	return &Message{Body: data, Format: "JSON"}, nil
+}
+
+// Sender Struct
+type Sender struct{}
+
+// SendMessage Function
+func (m *Sender) SendMessage(builder MessageBuilder) (*Message, error) {
+	builder.SetHeader("Dear Rangga")
+	builder.SetBody("Hello, How are you")
+	builder.SetFooter("Regards, Adhitya")
+
+	return builder.BuildMessage()
+}
+
+func main() {
+	sender := &Sender{}
+
+	jsonMessage, err := sender.SendMessage(&MessageStruct{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(jsonMessage.Body))
+}
